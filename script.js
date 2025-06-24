a373a735557bdd2e6c085dddfac93c23a0770751
Piter0055 <pedro.eduardo.silva13@escola.pr.gov.br>
2025-06-24 15:07:45 -0300

    ,.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function processaTexto (texto) {
     let palavras = texto.split(/\P{L}+/u);
     const frequencias  = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort();
+    console.log(ordenadas)
     return plalavras;
 }
 
